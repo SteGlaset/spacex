30f420a093758cf3dc1f6775420407deb02c4c15
SteGlaset <90640042+SteGlaset@users.noreply.github.com>
2023-08-01 00:47:14 +0300

    chore: add prettier

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.tsx',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'index_bundle.js',
+  entry: './src/index.tsx',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    target: 'web',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        open: true,
-        hot: true,
-        liveReload: true,
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: 'ts-loader',
-            }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public', 'index.html')
-        })
-    ]
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
 };

93a6ecfc43e949b90317797a6fbd7640e83db8c4
SteGlaset <90640042+SteGlaset@users.noreply.github.com>
2023-09-03 03:11:02 +0300

    refactor: move sorting logic into SortButton component

--- src/components/SortButton/SortButton.tsx
+++ src/components/SortButton/SortButton.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Button from '../UI/Button/Button';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { missionsApi } from '../../api/missionsApi';
+import { sort } from '../../redux/payload/payloadSlice';
+import { RootState } from '../../redux/store';
 
-interface SortButtonProps {
-  onSort: () => void;
-}
-
-const SortButton: React.FC<SortButtonProps> = ({ onSort }) => {
+const SortButton = () => {
   const dispatch = useDispatch();
+  const sortState = useSelector(
+    (state: RootState) => state.payload.options.sort
+  );
+
   const handleSort = () => {
     dispatch(missionsApi.util.resetApiState());
-    onSort();
+    dispatch(
+      sort({
+        flight_number: sortState?.flight_number === 'desc' ? 'asc' : 'desc',
+      })
+    );
   };
 
   return <Button onClick={handleSort}>Sort by Year</Button>;
